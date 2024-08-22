fb82aa432e4392f429974242d6f58ce312ec6418
Grzegorz Grunert <grzegorzgrunert@gmail.com>
2024-08-22 19:47:07 +0200

    feat: added libraries and logic for servo

---
+++ .pio/libdeps/nodemcu-32s/Adafruit_ESP32_SH1106/README.md
@@ -0,0 +1,17 @@
+Adafruit_SH1106
+===============
+
+Adafruit graphic library for SH1106 driver lcds.
+
+some small oled lcd use SH1106 driver.
+
+
+SH1106 driver similar to SSD1306. thus, just change the display() method. 
+ 
+However, SH1106 driver don't provide several functions such as scroll commands.
+
+
+Contains soem fixes for the ESP32 platform
+
+ Adafruit-GFX-Library
+ https://github.com/adafruit/Adafruit-GFX-Library
--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,66 @@
 #include <wifi/wifi-service.h>
 #include <services/pin-service.h>
 #include <http/http-server.h>
+#include <Adafruit_SH1106.h>
+#include <Adafruit_GFX.h>
+#include <ESP32Servo.h>
 
 // If you want to register a pin define it first as below:
-#define LED_RED_DIOD 25
-#define USB_BAUD_RATE 921600
+#define SERVO_PIN 23
+#define BTN_PIN 19
 
 // TODO: User should pass these values instead of hardcoding it here
-#define SSID "gregandgrun"
-#define PASSWORD "N1epam1etam"
+#define SSID "***"
+#define PASSWORD "***"
+
+#define OLED_RESET -1
+Adafruit_SH1106 display(OLED_RESET);
+Servo myServo;  
+
+bool buttonPressed = false;
 
 void setup() {
-  Serial.begin(USB_BAUD_RATE);
 
-  registerPin(LED_RED_DIOD, OUTPUT);
+  // OLED Display Configuration
+  display.begin(SH1106_I2C_ADDRESS, 0x3C);  // The I2C address is specified here
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("Hello, world!");
+  display.display();
+  
+  // Servo configuration
+  myServo.attach(SERVO_PIN);
+  myServo.write(0);
+
+  // Buttons configuration
+  registerPin(BTN_PIN, INPUT_PULLUP);
+
+  // Other services Configuration
   initWifi(SSID, PASSWORD);
   initHttpServer();
 }
  
 void loop() {
-  handleClient();
+  //handleClient();
+
+  if (digitalRead(BTN_PIN) == LOW && !buttonPressed) {
+    buttonPressed = true;  
+    display.setCursor(0,0);
+    display.clearDisplay();
+    display.print("Button is pressed");
+    display.display();
+
+    myServo.write(90);
+  }
+  if (digitalRead(BTN_PIN) == HIGH) {
+    buttonPressed = false;
+    display.setCursor(0,0);
+    display.clearDisplay();
+    display.print("Button is not pressed");
+    display.display();
+
+    myServo.write(5);
+  }
 }
